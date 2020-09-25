00efa1cfc56fdd701c32d02e18e9bcb0c590138f
grigori <gbarbachov@bktaxcpa.com>
2020-09-25 02:30:00 -0700

    Table View and Table Cell added

--- Flix/MovieCell.swift
+++ Flix/MovieCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class MovieCell: UITableViewCell {
 
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
